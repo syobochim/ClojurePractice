335b61ecad005f5a469c4c50322ec2cbc5762afb
syobochim <MarilynMonroe0226@gmail.com>
2014-09-29 08:23:08 +0900

    score完成

--- breaker/src/breaker/core.clj
+++ breaker/src/breaker/core.clj
@@ -1,6 +1,23 @@
-(ns breaker.core)
+(ns breaker.core
+  (:require [clojure.data :as data]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn exact-matches
+  "Given two collections, return the number of position where
+  the collections contain equal items."
+  [c1 c2]
+  (let [[_ _ matches] (data/diff c1 c2)]
+    (count (remove nil? matches))))
+
+(defn unordered-matches
+  "Given two collections, return a map where each key is an items
+  in both collections, and each value is the number of times the
+  value occurs in the collection with fewest occurrences."
+  [c1 c2]
+  (let [f1 (select-keys (frequencies c1) c2)
+        f2 (select-keys (frequencies c2) c1)]
+    (merge-with f1 f2)))
+
+(defn score [c1 c2]
+  (let [exact (exact-matches c1 c2)
+        unordered (apply + (vals (unordered-matches c1 c2)))]
+    {:exact exact :unordered (- unordered exact)}))

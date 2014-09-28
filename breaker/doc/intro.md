010c77a0a5cd92d8543eea6d5d3f78235f96958f
syobochim <MarilynMonroe0226@gmail.com>
2014-09-28 22:02:25 +0900

    create breaker PJ

---
+++ breaker/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to breaker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ breaker/project.clj
@@ -0,0 +1,3 @@
+(defproject breaker "0.1.0-SNAPSHOT"
+  :description "Clojure Breaker"
+  :dependencies [[org.clojure/clojure "1.6.0"]])

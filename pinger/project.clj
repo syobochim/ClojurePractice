3d8f4154fd480cef0a77df1ef440a748d927b8f0
syobochim <MarilynMonroe0226@gmail.com>
2014-09-28 12:43:59 +0900

    project.cljを変更

--- pinger/project.clj
+++ pinger/project.clj
@@ -1,6 +1,3 @@
-(defproject pinger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject pinger "0.0.1-SNAPSHOT"
+  :description "A website available tester"
   :dependencies [[org.clojure/clojure "1.6.0"]])

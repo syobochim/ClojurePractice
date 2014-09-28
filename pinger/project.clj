c638e87d4951bef57d41a747eeafed42602286cb
syobochim <MarilynMonroe0226@gmail.com>
2014-09-28 13:10:01 +0900

    1分ごとにURLにアクセスできるかチェッする

--- pinger/project.clj
+++ pinger/project.clj
@@ -1,3 +1,5 @@
 (defproject pinger "0.0.1-SNAPSHOT"
   :description "A website available tester"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main pinger.core)
+
--- pinger/src/pinger/core.clj
+++ pinger/src/pinger/core.clj
@@ -1,6 +1,23 @@
-(ns pinger.core)
+(ns pinger.core
+  (:import (java.net URL HttpURLConnection))
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn response-code [address]
+  (let [conn ^HttpURLConnection (.openConnection (URL. address))
+        code (.getResponseCode conn)]
+    (when (< code 400)
+      (-> conn .getInputStream .close))
+    code))
+
+(defn available? [address]
+  (= 200 (response-code address)))
+
+(defn -main []
+  (let [address '("https://github.com/syobochim"
+                  "https://twitter.com/syobochim"
+                  "http://google.com/badurl")]
+    (while true
+      (doseq [address address]
+        (println (available? address)))
+      (Thread/sleep (* 1000 60)))))
+
